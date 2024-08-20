1c04c95b7606ae0a69244681555ec475a082298a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-08-20 12:36:11 -0700

    elimindate warning

--- src/aqualuxury/__init__.py
+++ src/aqualuxury/__init__.py
@@ -7,7 +7,7 @@ __project_name__ = "aqualuxury"
 def main() -> int:
     result = requests.get("https://www.instagram.com/p/B2EtjT9hgvG/")
     c = result.content
-    soup = bs4.BeautifulSoup(c)
+    soup = bs4.BeautifulSoup(c, features="html.parser")
     metas = soup.find_all(attrs={"property": "og:image"})
     print(metas[0].attrs["content"])
     return 0
