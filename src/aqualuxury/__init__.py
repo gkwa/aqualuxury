3bd81ec8e75d55395f4c3e0b31613cbda9f79b73
Taylor Monacelli <taylormonacelli@gmail.com>
2024-08-20 12:33:56 -0700

    working with warning

--- src/aqualuxury/__init__.py
+++ src/aqualuxury/__init__.py
@@ -1,11 +1,13 @@
-import sys
-
-from . import main2
+import bs4
+import requests
 
 __project_name__ = "aqualuxury"
 
 
 def main() -> int:
-    out = main2.render_template("extended.j2")
-    sys.stdout.write(out)
+    result = requests.get("https://www.instagram.com/p/B2EtjT9hgvG/")
+    c = result.content
+    soup = bs4.BeautifulSoup(c)
+    metas = soup.find_all(attrs={"property": "og:image"})
+    print(metas[0].attrs["content"])
     return 0
